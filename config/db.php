5cded1675cb3c1b7ceea565a2cf9692ea3f26c0d
juggkit <feidong1112@gmail.com>
2018-11-30 18:53:41 +0800

    ignore

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=wechatMp',
-    'username' => 'root',
-    'password' => 'root@123',
+    'dsn' => 'mysql:host=localhost;dbname=',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
